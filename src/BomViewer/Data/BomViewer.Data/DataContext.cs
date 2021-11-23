f0d671ddf4001e5539bed369e0d89a9e1a7f0f7f
Sergey Voytovich <s.voytovich@outlook.com>
2021-11-23 09:02:10 -0800

    Data context added

---
+++ src/BomViewer/Data/BomViewer.Data/DataContext.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using BomViewer.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace BomViewer.Data
+{
+    /// <summary>
+    /// Data context
+    /// </summary>
+    internal class DataContext : DbContext
+    {
+        #region Properties
+
+        /// <summary>
+        /// Groups
+        /// </summary>
+        public DbSet<GroupEntity> Groups { get; set; }
+
+        /// <summary>
+        /// Parts
+        /// </summary>
+        public DbSet<PartEntity> Parts { get; set; }
+
+        #endregion
+
+
+        #region Methods
+
+        /// <summary>
+        /// See <see cref="DbContext.OnConfiguring"/>
+        /// </summary>
+        /// <param name="optionsBuilder"></param>
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder
+                .LogTo(Console.WriteLine)
+                .EnableSensitiveDataLogging();
+        }
+
+        #endregion
+    }
+}
