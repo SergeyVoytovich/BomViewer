129920cc95aef09f6e120e90eeb9915dec8b93e9
Sergey Voytovich <s.voytovich@outlook.com>
2021-11-23 11:07:23 -0800

    Seed builder declared

---
+++ src/BomViewer/Data/BomViewer.Data/Seed/ISeedBuilder.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace BomViewer.Data.Seed
+{
+    internal interface ISeedBuilder
+    {
+        Task BuildAsync(ModelBuilder modelBuilder);
+    }
+}
---
+++ src/BomViewer/Data/BomViewer.Data/Seed/SeedBuilder.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace BomViewer.Data.Seed
+{
+    internal class SeedBuilder : ISeedBuilder
+    {
+        public Task BuildAsync(ModelBuilder modelBuilder)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
