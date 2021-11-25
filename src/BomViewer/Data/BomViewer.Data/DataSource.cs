28ceab4171ab722c23e8c7a13e5a4a2995408db1
Sergey Voytovich <s.voytovich@outlook.com>
2021-11-25 11:01:31 -0800

    DataSource implemented

---
+++ src/BomViewer/Data/BomViewer.Data/DataSource.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using AutoMapper;
+using BomViewer.Data.Mapping;
+using BomViewer.Data.Repositories;
+
+namespace BomViewer.Data
+{
+    public class DataSource : IDataSource
+    {
+        #region Properties
+
+        public IGroupsRepository Groups { get; }
+        public IPartsRepository Parts { get; }
+
+        #endregion
+
+
+        #region Constructors
+
+        public DataSource()
+            : this(new DataContext(), MapperFactory.Init())
+        {
+        }
+
+        internal DataSource(DataContext context, IMapper mapper)
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException(nameof(context));
+            }
+
+            Groups = new GroupsRepository(context.Groups.AsQueryable(), mapper);
+            Parts = new PartsRepository(context.Parts.AsQueryable(), mapper);
+        }
+
+        #endregion
+    }
+}
