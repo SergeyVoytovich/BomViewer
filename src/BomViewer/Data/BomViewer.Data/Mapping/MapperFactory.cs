11b022c5fc4e57f1e111fb47b538960041c4f3fa
Sergey Voytovich <s.voytovich@outlook.com>
2021-11-25 01:33:07 -0800

    Seed mapping profile added

---
+++ src/BomViewer/Data/BomViewer.Data/Mapping/MapperFactory.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+
+namespace BomViewer.Data.Mapping
+{
+    public class MapperFactory
+    {
+        public static IMapper Init()
+        {
+            var config = new MapperConfiguration(c =>
+            {
+                c.AddProfile<SeedProfile>();
+            });
+
+            config.AssertConfigurationIsValid();
+            return config.CreateMapper();
+        }
+    }
+}
---
+++ src/BomViewer/Data/BomViewer.Data/Mapping/SeedProfile.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using BomViewer.Data.Entities;
+using BomViewer.Data.Seed;
+
+namespace BomViewer.Data.Mapping
+{
+    public class SeedProfile : Profile
+    {
+        public SeedProfile()
+        {
+            CreateMap<GroupSeed, GroupEntity>()
+                .ForMember(dst => dst.Name, opt => opt.MapFrom(src => src.ComponentName))
+                ;
+
+            CreateMap<PartSeed, PartEntity>()
+                .ForMember(dst => dst.Item, opt => opt.MapFrom(src => src.Item))
+                .ForMember(dst => dst.Material, opt => opt.MapFrom(src => src.Material))
+                .ForMember(dst => dst.Number, opt => opt.MapFrom(src => src.PartNumber))
+                .ForMember(dst => dst.Title, opt => opt.MapFrom(src => src.Title))
+                .ForMember(dst => dst.Type, opt => opt.MapFrom(src => src.Type))
+                ;
+        }
+    }
+}
