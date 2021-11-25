bec6295a0aa3f72da7d1dd046f659980d6783533
Sergey Voytovich <s.voytovich@outlook.com>
2021-11-25 11:16:40 -0800

    Application project added

---
+++ src/BomViewer/BomViewer.Common/Application/IApplication.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using BomViewer.DomainObjects;
+
+namespace BomViewer.Application
+{
+    /// <summary>
+    /// Application
+    /// </summary>
+    public interface IApplication
+    {
+        /// <summary>
+        /// Get groups
+        /// </summary>
+        /// <returns></returns>
+        IList<IGroup> GetGroups();
+
+        /// <summary>
+        /// Get parts for groups
+        /// </summary>
+        /// <param name="groups">List of groups</param>
+        /// <returns></returns>
+        IList<IPart> GetParts(IList<IGroup> groups);
+    }
+}
