d540ed8f60fd76b6b13a9b33f5ea5f59202bc6bf
Sergey Voytovich <s.voytovich@outlook.com>
2021-11-25 08:47:22 -0800

    Unit test for SeedBuilder implemented

--- src/BomViewer/Data/BomViewer.Data.Tests/Seed/SeedBuilderTests.cs
+++ src/BomViewer/Data/BomViewer.Data.Tests/Seed/SeedBuilderTests.cs
@@ -1,5 +1,11 @@
-﻿using BomViewer.Data.Seed;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BomViewer.Data.Entities;
+using BomViewer.Data.Mapping;
+using BomViewer.Data.Seed;
 using BomViewer.Data.Seed.Readers;
+using Dasync.Collections;
 using FluentAssertions;
 using Moq;
 using Xunit;
@@ -8,13 +14,250 @@ namespace BomViewer.Data.Tests.Seed
 {
     public class SeedBuilderTests
     {
-        //[Fact]
-        //public void Constructor()
-        //{
-        //    var readers = new Mock<IReadersFactory>();
-        //    var builder = new Mock<SeedBuilder>() {CallBase = true};
+        [Fact]
+        public void Constructor()
+        {
+            // Prepare
+            var mapper = MapperFactory.Init();
+            var readers = new Mock<IReadersFactory>();
+            var entityBuilder = new Mock<IEntityBuilder>();
+            var builder = new Mock<SeedBuilder>(readers.Object, mapper, entityBuilder.Object) {CallBase = true};
 
-        //    builder.Should().NotBeNull();
-        //}
+            // Assert
+            builder.Object.Should().NotBeNull();
+        }
+
+        [Fact]
+        public async Task BuildAsync()
+        {
+            // Prepare
+            var mapper = MapperFactory.Init();
+            var readers = new Mock<IReadersFactory>();
+            var entityBuilder = new Mock<IEntityBuilder>();
+            var builder = new Mock<SeedBuilder>(readers.Object, mapper, entityBuilder.Object) { CallBase = true };
+            var groups = new List<GroupEntity>();
+            builder.Setup(b => b.BuildGroupsAsync()).ReturnsAsync(groups);
+            builder.Setup(b => b.BuildPartsAsync(It.IsAny<IList<GroupEntity>>())).Returns(Task.CompletedTask);
+
+            //Action
+            await builder.Object.BuildAsync();
+
+            // Assert
+            builder.Verify(b => b.BuildGroupsAsync(), Times.Once);
+            builder.Verify(b => b.BuildPartsAsync(It.Is<IList<GroupEntity>>(i => Equals(i, groups))), Times.Once);
+        }
+
+
+
+        #region BuildGroup
+
+        [Fact]
+        public async Task BuildGroupsAsync()
+        {
+            // Prepare
+            var mapper = MapperFactory.Init();
+            var reader = new Mock<IReader<GroupSeed>>();
+            var readers = new Mock<IReadersFactory>();
+            readers.Setup(r => r.Groups(It.IsAny<string>())).Returns(reader.Object);
+            var entityBuilder = new Mock<IEntityBuilder>();
+            var builder = new Mock<SeedBuilder>(readers.Object, mapper, entityBuilder.Object) { CallBase = true };
+            var groups = new List<GroupEntity>();
+            builder.Setup(b => b.BuildGroupsAsync(It.IsAny<IReader<GroupSeed>>())).ReturnsAsync(groups);
+
+            //Action
+            await builder.Object.BuildGroupsAsync();
+
+            // Assert
+            builder.Verify(b => b.BuildGroupsAsync(It.Is<IReader<GroupSeed>>(r => r == reader.Object)));
+            readers.Verify(r => r.Groups(It.Is<string>(s => s == SeedBuilder.GroupsFileName)));
+        }
+
+        [Fact]
+        public async Task BuildGroupsAsync_ByReader()
+        {
+            // Prepare
+            var mapper = MapperFactory.Init();
+            var reader = new Mock<IReader<GroupSeed>>();
+            var asyncEnumerable = new AsyncEnumerable<GroupSeed>(yield => yield.ReturnAsync(new GroupSeed()));
+            reader.Setup(r => r.ReadAsync()).Returns(asyncEnumerable);
+            var readers = new Mock<IReadersFactory>();
+            var entityBuilder = new Mock<IEntityBuilder>();
+            var builder = new Mock<SeedBuilder>(readers.Object, mapper, entityBuilder.Object) { CallBase = true };
+            var groups = new List<GroupEntity>();
+            builder.Setup(b => b.BuildGroupsAsync(It.IsAny<IAsyncEnumerable<GroupSeed>>())).ReturnsAsync(groups);
+
+            //Action
+            await builder.Object.BuildGroupsAsync(reader.Object);
+
+            // Assert
+            builder.Verify(b => b.BuildGroupsAsync(It.Is<IAsyncEnumerable<GroupSeed>>(e => e == asyncEnumerable)));
+            reader.Verify(r => r.ReadAsync());
+        }
+
+        [Fact]
+        public async Task BuildGroupsAsync_ByAsyncEnumerable()
+        {
+            // Prepare
+            var mapper = MapperFactory.Init();
+            var asyncEnumerable = new AsyncEnumerable<GroupSeed>(yield => yield.ReturnAsync(new GroupSeed()));
+            var readers = new Mock<IReadersFactory>();
+            var entityBuilder = new Mock<IEntityBuilder>();
+            var builder = new Mock<SeedBuilder>(readers.Object, mapper, entityBuilder.Object) { CallBase = true };
+            var groups = new List<GroupEntity>();
+            builder.Setup(b => b.BuildGroupsAsync(It.IsAny<IAsyncEnumerator<GroupSeed>>())).ReturnsAsync(groups);
+
+            //Action
+            await builder.Object.BuildGroupsAsync(asyncEnumerable);
+
+            // Assert
+            builder.Verify(b => b.BuildGroupsAsync(It.IsAny<IAsyncEnumerator<GroupSeed>>()));
+        }
+
+        [Fact]
+        public async Task BuildGroupsAsync_ByAsyncEnumerator()
+        {
+            // Prepare
+            var mapper = MapperFactory.Init();
+            var asyncEnumerable = new AsyncEnumerable<GroupSeed>(yield => yield.ReturnAsync(new GroupSeed()));
+            var enumerator = asyncEnumerable.GetAsyncEnumerator();
+            var readers = new Mock<IReadersFactory>();
+            var entityBuilder = new Mock<IEntityBuilder>();
+            var builder = new Mock<SeedBuilder>(readers.Object, mapper, entityBuilder.Object) { CallBase = true };
+            builder.Setup(b => b.BuildGroup(It.IsAny<GroupSeed>(), It.IsAny<IList<GroupEntity>>())).Returns(new GroupEntity());
+
+            //Action
+            var actual = await builder.Object.BuildGroupsAsync(enumerator);
+
+            // Assert
+            actual.Should().HaveCount(1);
+            builder.Verify(b => b.BuildGroup(It.Is<GroupSeed>(g => g != null), It.IsAny<IList<GroupEntity>>()));
+        }
+
+        [Fact]
+        public void BuildGroup()
+        {
+            // Prepare
+            var mapper = MapperFactory.Init();
+            var readers = new Mock<IReadersFactory>();
+            var entityBuilder = new Mock<IEntityBuilder>();
+            entityBuilder.Setup(b => b.HasData(It.IsAny<It.IsAnyType>()));
+            var builder = new Mock<SeedBuilder>(readers.Object, mapper, entityBuilder.Object) { CallBase = true };
+            var parent = new GroupEntity { Name = "test" };
+
+            //Action
+
+            var actual = builder.Object.BuildGroup(new GroupSeed{ParentName = "test"}, new []{parent});
+
+            // Assert
+            actual.Should().NotBeNull();
+            actual.Parent.Should().NotBeNull().And.Be(parent);
+            entityBuilder.Verify(b => b.HasData(It.IsAny<It.IsAnyType>()));
+        }
+
+        #endregion
+
+
+        #region Build part
+
+        [Fact]
+        public async Task BuildPartsAsync()
+        {
+            // Prepare
+            var mapper = MapperFactory.Init();
+            var readers = new Mock<IReadersFactory>();
+            readers.Setup(r => r.Parts(It.IsAny<string>())).Returns(Mock.Of<IReader<PartSeed>>());
+            var entityBuilder = new Mock<IEntityBuilder>();
+            var builder = new Mock<SeedBuilder>(readers.Object, mapper, entityBuilder.Object) { CallBase = true };
+            builder.Setup(b => b.BuildPartsAsync(It.IsAny<IList<GroupEntity>>(), It.IsAny<IReader<PartSeed>>())).Returns(Task.CompletedTask);
+
+            //Action
+
+            await builder.Object.BuildPartsAsync(new List<GroupEntity>());
+
+            // Assert
+            readers.Verify(r => r.Parts(It.Is<string>(s => s == SeedBuilder.PartsFileName)));
+            builder.Verify(b => b.BuildPartsAsync(It.IsAny<IList<GroupEntity>>(), It.Is<IReader<PartSeed>>(r => r != null)));
+        }
+
+        [Fact]
+        public async Task BuildPartsAsync_ByReader()
+        {
+            // Prepare
+            var mapper = MapperFactory.Init();
+            var reader = new Mock<IReader<PartSeed>>();
+            reader.Setup(r => r.ReadAsync()).Returns(new AsyncEnumerable<PartSeed>(yield => yield.ReturnAsync(new PartSeed())));
+            var readers = new Mock<IReadersFactory>();
+            var entityBuilder = new Mock<IEntityBuilder>();
+            var builder = new Mock<SeedBuilder>(readers.Object, mapper, entityBuilder.Object) { CallBase = true };
+            builder.Setup(b => b.BuildPartsAsync(It.IsAny<IList<GroupEntity>>(), It.IsAny<IAsyncEnumerable<PartSeed>>())).Returns(Task.CompletedTask);
+
+            //Action
+
+            await builder.Object.BuildPartsAsync(new List<GroupEntity>(), reader.Object);
+
+            // Assert
+            builder.Verify(b => b.BuildPartsAsync(It.IsAny<IList<GroupEntity>>(), It.IsAny<IAsyncEnumerable<PartSeed>>()));
+            reader.Verify(r => r.ReadAsync());
+        }
+
+        [Fact]
+        public async Task BuildPartsAsync_ByAsyncEnumerable()
+        {
+            // Prepare
+            var mapper = MapperFactory.Init();
+            var enumerable = new AsyncEnumerable<PartSeed>(yield => yield.ReturnAsync(new PartSeed()));
+            var readers = new Mock<IReadersFactory>();
+            var entityBuilder = new Mock<IEntityBuilder>();
+            var builder = new Mock<SeedBuilder>(readers.Object, mapper, entityBuilder.Object) { CallBase = true };
+            builder.Setup(b => b.BuildPartsAsync(It.IsAny<IList<GroupEntity>>(), It.IsAny<IAsyncEnumerator<PartSeed>>())).Returns(Task.CompletedTask);
+
+            //Action
+
+            await builder.Object.BuildPartsAsync(new List<GroupEntity>(), enumerable);
+
+            // Assert
+            builder.Verify(b => b.BuildPartsAsync(It.IsAny<IList<GroupEntity>>(), It.IsAny<IAsyncEnumerator<PartSeed>>()));
+        }
+
+        [Fact]
+        public async Task BuildPartsAsync_ByAsyncEnumerator()
+        {
+            // Prepare
+            var mapper = MapperFactory.Init();
+            var enumerator = new AsyncEnumerable<PartSeed>(yield => yield.ReturnAsync(new PartSeed())).GetAsyncEnumerator();
+            var readers = new Mock<IReadersFactory>();
+            var entityBuilder = new Mock<IEntityBuilder>();
+            var builder = new Mock<SeedBuilder>(readers.Object, mapper, entityBuilder.Object) { CallBase = true };
+            builder.Setup(b => b.BuildPart(It.IsAny<IList<GroupEntity>>(), It.IsAny<PartSeed>(), It.IsAny<int>()));
+
+            //Action
+
+            await builder.Object.BuildPartsAsync(new List<GroupEntity>(), enumerator);
+
+            // Assert
+            builder.Verify(b => b.BuildPart(It.IsAny<IList<GroupEntity>>(), It.IsAny<PartSeed>(), It.IsAny<int>()), Times.Exactly(1));
+        }
+
+        [Fact]
+        public void BuildPart()
+        {
+            // Prepare
+            var mapper = MapperFactory.Init();
+            var readers = new Mock<IReadersFactory>();
+            var entityBuilder = new Mock<IEntityBuilder>();
+            entityBuilder.Setup(b => b.HasData(It.IsAny<It.IsAnyType>()));
+            var builder = new Mock<SeedBuilder>(readers.Object, mapper, entityBuilder.Object) { CallBase = true };
+            var groups = new List<GroupEntity> {new GroupEntity {Name = "test"}};
+            var seed = new PartSeed {Name = "test"};
+            const int id = 1;
+
+            //Action
+            builder.Object.BuildPart(groups, seed, id);
+
+            // Assert
+            entityBuilder.Verify(b => b.HasData(It.Is<PartEntity>(p => p.Id == id + 1 && p.Group == groups.First())));
+        }
+
+        #endregion
     }
 }
--- src/BomViewer/Data/BomViewer.Data/Seed/SeedBuilder.cs
+++ src/BomViewer/Data/BomViewer.Data/Seed/SeedBuilder.cs
@@ -1,13 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 using AutoMapper;
 using BomViewer.Data.Entities;
 using BomViewer.Data.Seed.Readers;
-
-[assembly: InternalsVisibleTo("BomViewer.Data.Tests")]
 
 namespace BomViewer.Data.Seed
 {
@@ -18,6 +15,14 @@ namespace BomViewer.Data.Seed
         private readonly IReadersFactory _readers;
         private readonly IMapper _mapper;
         private readonly IEntityBuilder _builder;
+
+        #endregion
+
+
+        #region Public constants
+
+        public const string GroupsFileName = "bom.csv";
+        public const string PartsFileName = "part.csv";
 
         #endregion
 
@@ -38,26 +43,32 @@ namespace BomViewer.Data.Seed
 
         public async Task BuildAsync()
         {
-            var groups = await BuildGroups();
-            await BuildParts(groups);
+            var groups = await BuildGroupsAsync();
+            await BuildPartsAsync(groups);
         }
 
         #region BuildGroups
 
-        internal virtual Task<IList<GroupEntity>> BuildGroups() 
-            => BuildGroups(_readers.Groups("").ReadAsync().GetAsyncEnumerator());
+        internal virtual Task<IList<GroupEntity>> BuildGroupsAsync() 
+            => BuildGroupsAsync(_readers.Groups(GroupsFileName));
 
-        internal virtual async Task<IList<GroupEntity>> BuildGroups(IAsyncEnumerator<GroupSeed> groups)
+        internal virtual Task<IList<GroupEntity>> BuildGroupsAsync(IReader<GroupSeed> reader)
+            => BuildGroupsAsync(reader?.ReadAsync());
+
+        internal virtual Task<IList<GroupEntity>> BuildGroupsAsync(IAsyncEnumerable<GroupSeed> seed)
+            => BuildGroupsAsync(seed?.GetAsyncEnumerator());
+
+        internal virtual async Task<IList<GroupEntity>> BuildGroupsAsync(IAsyncEnumerator<GroupSeed> seed)
         {
-            if (groups == null)
+            if (seed == null)
             {
-                throw new ArgumentNullException(nameof(groups));
+                throw new ArgumentNullException(nameof(seed));
             }
 
             var result = new List<GroupEntity>();
-            while (await groups.MoveNextAsync())
+            while (await seed.MoveNextAsync())
             {
-                BuildGroup(groups.Current, result);
+                result.Add(BuildGroup(seed.Current, result));
             }
 
             return result;
@@ -83,26 +94,32 @@ namespace BomViewer.Data.Seed
 
         #region BuildParts
 
-        internal virtual Task BuildParts(IList<GroupEntity> groups)
-            => BuildParts(groups, _readers.Parts("").ReadAsync().GetAsyncEnumerator());
+        internal virtual Task BuildPartsAsync(IList<GroupEntity> groups)
+            => BuildPartsAsync(groups, _readers.Parts(PartsFileName));
 
-        internal virtual async Task BuildParts(IList<GroupEntity> groups, IAsyncEnumerator<PartSeed> parts)
+        internal virtual Task BuildPartsAsync(IList<GroupEntity> groups, IReader<PartSeed> reader)
+            => BuildPartsAsync(groups, reader.ReadAsync());
+
+        internal virtual Task BuildPartsAsync(IList<GroupEntity> groups, IAsyncEnumerable<PartSeed> seed)
+            => BuildPartsAsync(groups, seed.GetAsyncEnumerator());
+
+        internal virtual async Task BuildPartsAsync(IList<GroupEntity> groups, IAsyncEnumerator<PartSeed> parts)
         {
             var id = 0;
             while (await parts.MoveNextAsync())
             {
-                id = BuildPart(groups, parts, id);
+                id = BuildPart(groups, parts.Current, id);
             }
         }
 
-        internal virtual int BuildPart(IEnumerable<GroupEntity> groups, IAsyncEnumerator<PartSeed> parts, int id)
+        internal virtual int BuildPart(IEnumerable<GroupEntity> groups, PartSeed part, int id)
         {
-            var entity = _mapper.Map<PartEntity>(parts.Current);
-            entity.Id = ++id;
-            entity.Group = groups.FirstOrDefault(g => string.Equals(g.Name, parts.Current.Name, StringComparison.OrdinalIgnoreCase));
+            var entity = _mapper.Map<PartEntity>(part);
+            entity.Id = id + 1;
+            entity.Group = groups.FirstOrDefault(g => string.Equals(g.Name, part.Name, StringComparison.OrdinalIgnoreCase));
 
             _builder.HasData(entity);
-            return id;
+            return entity.Id;
         }
 
         #endregion
