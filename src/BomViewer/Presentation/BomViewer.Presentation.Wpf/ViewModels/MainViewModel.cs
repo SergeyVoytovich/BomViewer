0dc97ee8f0692d0432ee4aa66e9e5b6fad892bf0
Sergey Voytovich <s.voytovich@outlook.com>
2021-11-25 11:45:24 -0800

    View models factory added

---
+++ src/BomViewer/Presentation/BomViewer.Presentation.Wpf/ViewModels/MainViewModel.cs
@@ -0,0 +1,11 @@
+﻿using BomViewer.Application;
+
+namespace BomViewer.Presentation.Wpf.ViewModels
+{
+    public class MainViewModel : ViewModelBase
+    {
+        public MainViewModel(IApplication application) : base(application)
+        {
+        }
+    }
+}
---
+++ src/BomViewer/Presentation/BomViewer.Presentation.Wpf/ViewModels/ViewModelsFactory.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using BomViewer.Application;
+using BomViewer.Data;
+
+namespace BomViewer.Presentation.Wpf.ViewModels
+{
+    /// <summary>
+    /// View models factory
+    /// </summary>
+    public class ViewModelsFactory
+    {
+        #region Fields
+
+        private readonly IApplication _application;
+
+        #endregion
+
+
+        #region Constructors
+
+        /// <summary>
+        /// Initialize a new instance
+        /// </summary>
+        public ViewModelsFactory()
+            : this(new DataSource())
+        {
+
+        }
+
+        /// <summary>
+        /// Initialize a new instance
+        /// </summary>
+        public ViewModelsFactory(IDataSource dataSource)
+            : this(new Application.Application(dataSource))
+        {
+
+        }
+
+        /// <summary>
+        /// Initialize a new instance
+        /// </summary>
+        public ViewModelsFactory(IApplication application)
+        {
+            _application = application ?? throw new ArgumentNullException(nameof(application));
+        }
+
+        #endregion
+
+
+        #region Properties
+
+        /// <summary>
+        /// Main view model
+        /// </summary>
+        public MainViewModel Main => new(_application);
+
+        #endregion
+    }
+}
